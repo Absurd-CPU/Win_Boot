3eb67989d1c2f025d549b3c042b94b6f94496d75
MOhammad_H_Sabzevari <mohammadsabzevarih@gmail.com>
2026-09-19 13:42:39 +0330

    Update kernel.asm

--- Basics-example/kernel.asm
+++ Basics-example/kernel.asm
@@ -1,1 +1,29 @@
+[org 0x1000]
+bits 16
 
+start:
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    mov si, msg_kernel
+    call print
+
+    mov si, msg_HellO
+    call print
+
+    jmp $
+
+print:
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0E
+    int 0x10
+    jmp print
+.done:
+    ret
+
+msg_kernel db "Success: Kernel Loaded at 0x1000!", 13, 10, 0
+msg_HellO  db "Hello !.............."
+times 2048-($-$$) db 0
